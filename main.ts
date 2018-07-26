c1d2b17caf4bb807c6116fcd1994fd0453424073
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-07-26 19:29:55 +0800

    Add electron-reload

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
 import { app, ipcMain, ipcRenderer, BrowserWindow, screen } from "electron";
-
+import * as path from "path";
+import * as url from "url";
+require("electron-reload")(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+});
 let win;
-
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadFile("index.html");
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "./dist/electron-angular/index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
   win.webContents.openDevTools();
   win.on("closed", () => {
     app.quit();
