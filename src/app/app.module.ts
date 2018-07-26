5b68b389224a3e4b9803bdb89a9369298fd49d69
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-07-27 00:11:01 +0800

    Add electron service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./modules/app-routing.module";
 
+import { ElectronService } from "./services/electron.service";
+
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./components/home/home.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [AppRoutingModule, HttpClientModule, BrowserModule],
-  providers: [],
+  providers: [ElectronService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/services/electron.service.ts
+++ src/app/services/electron.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from "@angular/core";
-
+import { ipcRenderer, webFrame, remote } from "electron";
+import * as childProcess from "child_process";
+import * as fs from "fs";
 @Injectable({
   providedIn: "root"
 })
 export class ElectronService {
-  constructor() {}
+  ipcRenderer: typeof ipcRenderer;
+  webFrame: typeof webFrame;
+  remote: typeof remote;
+  childProcess: typeof childProcess;
+  fs: typeof fs;
+  isElectron: any;
+
+  constructor() {
+    this.checkElectron();
+    // Conditional imports
+    if (this.isElectron) {
+      this.ipcRenderer = (<any>window).require("electron").ipcRenderer;
+      this.webFrame = (<any>window).require("electron").webFrame;
+      this.remote = (<any>window).require("electron").remote;
+      this.childProcess = (<any>window).require("electron").childProcess;
+      this.fs = (<any>window).require("electron").fs;
+    }
+  }
+
+  private checkElectron() {
+    this.isElectron = window && (<any>window).process && (<any>window).process.type;
+  }
 }
