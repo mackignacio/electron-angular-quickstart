bdf6fac5f3e26e42ecd3b4f8cfa451afc9798270
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-07-27 14:36:32 +0800

    Add ElectronMain class

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 import { app, ipcMain, ipcRenderer, BrowserWindow, screen } from "electron";
 import * as path from "path";
 import * as url from "url";
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
+
 require("electron-reload")(__dirname, {
   electron: require(`${__dirname}/node_modules/electron`)
 });
 let win;
-process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600 });
   win.loadURL(
@@ -34,3 +35,63 @@ app.on("activate", () => {
     createWindow();
   }
 });
+class ElectronMain {
+  args: any;
+  serve: boolean;
+  mainWindow: BrowserWindow;
+
+  constructor() {
+    app.on("ready", this.createMainWindow);
+    require("electron-reload")(__dirname, {
+      electron: require(`${__dirname}/node_modules/electron`)
+    });
+    this.initApp();
+    this.initIpc();
+  }
+  checkElectronArgs() {
+    this.args = process.argv.slice(1);
+    this.serve = this.args.some(val => val === "--serve");
+    if (this.serve) {
+      require("electron-reload")(__dirname, {
+        electron: require(`${__dirname}/node_modules/electron`)
+      });
+    }
+  }
+
+  createMainWindow() {
+    this.mainWindow = new BrowserWindow({
+      x: 0,
+      y: 0
+    });
+    this.mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "/dist/electron-angular/index.html"),
+        protocol: "file:",
+        slashes: true
+      })
+    );
+    this.mainWindow.webContents.openDevTools();
+    this.mainWindow.on("closed", () => {
+      app.quit();
+    });
+  }
+
+  initApp() {
+    app.on("ready", this.createMainWindow);
+
+    app.on("window-all-closed", () => {
+      if (process.platform !== "darwin") {
+        app.quit();
+      }
+    });
+
+    app.on("activate", () => {
+      if (this.mainWindow === null) {
+        this.createMainWindow();
+      }
+    });
+  }
+  initIpc() {}
+}
+
+// export default new ElectronMain();
