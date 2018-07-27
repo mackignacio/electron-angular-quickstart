661df2db62636e8301154bed7a3deebb3ba357ac
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-07-27 23:27:13 +0800

    Refactor some codes

--- main.ts
+++ main.ts
@@ -13,28 +13,28 @@ class ElectronMain {
     this.initAppEvents();
     this.initIpc();
   }
-  
-  initApp(){
+
+  initApp() {
     this.checkElectronArgs();
     this.disableSecurityWarnings();
   }
-  
+
   initAppEvents() {
     app.on("ready", () => this.createMainWindow());
     app.on("window-all-closed", () => this.quitAppOnNonDarwin());
     app.on("activate", () => this.createDefaultWindow());
   }
-  
+
   initIpc() {
     ipcMain.on("event", (e, data) => this.ipcEventHandler(e, data));
   }
-  
+
   checkElectronArgs() {
     this.args = process.argv.slice(1);
     this.serve = this.args.some(val => val === "--serve");
     this.enableHotReload();
   }
-  
+
   enableHotReload() {
     if (this.serve) {
       require("electron-reload")(__dirname, {
@@ -52,7 +52,7 @@ class ElectronMain {
 
   createBrowserWindow(): BrowserWindow {
     return new BrowserWindow({
-     title: this.appTitle,
+      title: this.appTitle,
       fullscreen: true,
       minimizable: false,
       maximizable: false,
@@ -71,37 +71,34 @@ class ElectronMain {
       })
     );
   }
-  
+
   openWindowDevTools(electronWindow: BrowserWindow) {
     electronWindow.webContents.openDevTools();
   }
-  
+
   onWindowClosed(electronWindow: BrowserWindow) {
     electronWindow.on("closed", () => app.quit());
   }
-  
-  
+
   createDefaultWindow() {
     if (null === this.mainWindow) {
       this.createMainWindow();
     }
   }
-  
+
   quitAppOnNonDarwin() {
     if (process.platform !== "darwin") {
       app.quit();
     }
   }
-  
+
   ipcEventHandler(e: any, data: any) {
     console.log("[EVENT]:", "recieved from main process");
   }
-  
+
   disableSecurityWarnings() {
     process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
   }
-  
 }
 
 export default new ElectronMain();
-
