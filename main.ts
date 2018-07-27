ae80166c69fc55674913e9f6dfbcb3aa2f685cf1
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-07-27 23:13:10 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,17 +6,34 @@ class ElectronMain {
   args: any;
   serve: boolean;
   mainWindow: BrowserWindow;
+  
   constructor() {
-    this.checkElectronArgs();
-    this.disableSecurityWarnings();
-    this.enableHotReload();
+    this.initApp();
     this.initializeAppEvents();
     this.initIpc();
   }
+  
+  initApp(){
+    this.checkElectronArgs();
+    this.disableSecurityWarnings();
+  }
+  
+  initializeAppEvents() {
+    app.on("ready", () => this.createMainWindow());
+    app.on("window-all-closed", () => this.quitAppOnNonDarwin());
+    app.on("activate", () => this.createDefaultWindow());
+  }
+  
+  initIpc() {
+    ipcMain.on("event", (e, data) => this.ipcEventHandler(e, data));
+  }
+  
   checkElectronArgs() {
     this.args = process.argv.slice(1);
     this.serve = this.args.some(val => val === "--serve");
+    this.enableHotReload();
   }
+  
   enableHotReload() {
     if (this.serve) {
       require("electron-reload")(__dirname, {
@@ -31,12 +48,14 @@ class ElectronMain {
     this.openWindowDevTools(this.mainWindow);
     this.onWindowClosed(this.mainWindow);
   }
+
   createBrowserWindow(): BrowserWindow {
     return new BrowserWindow({
       x: 0,
       y: 0
     });
   }
+
   loadFromFile(electronWindow: BrowserWindow) {
     electronWindow.loadURL(
       url.format({
@@ -46,36 +65,36 @@ class ElectronMain {
       })
     );
   }
+  
   openWindowDevTools(electronWindow: BrowserWindow) {
     electronWindow.webContents.openDevTools();
   }
+  
   onWindowClosed(electronWindow: BrowserWindow) {
     electronWindow.on("closed", () => app.quit());
   }
-  initializeAppEvents() {
-    app.on("ready", () => this.createMainWindow());
-    app.on("window-all-closed", () => this.quitAppOnNonDarwin());
-    app.on("activate", () => this.createDefaultWindow());
-  }
+  
+  
   createDefaultWindow() {
     if (null === this.mainWindow) {
       this.createMainWindow();
     }
   }
+  
   quitAppOnNonDarwin() {
     if (process.platform !== "darwin") {
       app.quit();
     }
   }
-  initIpc() {
-    ipcMain.on("event", (e, data) => this.ipcEventHandler(e, data));
-  }
+  
   ipcEventHandler(e: any, data: any) {
     console.log("[EVENT]:", "recieved from main process");
   }
+  
   disableSecurityWarnings() {
     process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
   }
+  
 }
 
 export default new ElectronMain();
