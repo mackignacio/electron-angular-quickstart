490dfe54882f247149e24f3c91ff4f8c3befe61a
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-07-28 13:11:00 +0800

    Put v1.0.0 `main.ts` code until `ElectronMain()` is not stable

--- main.ts
+++ main.ts
@@ -2,6 +2,40 @@ import { app, ipcMain, ipcRenderer, BrowserWindow, screen } from "electron";
 import * as path from "path";
 import * as url from "url";
 
+require("electron-reload")(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+let win;
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "/dist/electron-angular/index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  win.webContents.openDevTools();
+  win.on("closed", () => {
+    app.quit();
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
 class ElectronMain {
   appTitle = "Electron Angular Quickstart";
   args: any;
@@ -101,4 +135,4 @@ class ElectronMain {
   }
 }
 
-export default new ElectronMain();
+// export default new ElectronMain();
